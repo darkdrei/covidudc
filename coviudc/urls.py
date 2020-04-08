877a1c9af14a2a96da906f7f21c195fb1cebe1df
Virgilio <vpadillar8@gmail.com>
2020-04-07 19:22:12 -0500

    quitando path para admin adicional

--- coviudc/urls.py
+++ coviudc/urls.py
@@ -22,7 +22,6 @@ from coviudc.views import IndexAdmin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', IndexAdmin.as_view(), name='index'),
 ]
