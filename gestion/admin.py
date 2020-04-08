a4eab1e51f3420025316990c3acb2d0db5279b63
Virgilio <vpadillar8@gmail.com>
2020-04-07 23:33:56 -0500

    agregando list_filter, inlines

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from gestion import models
 
-# Register your models here.
 
 class RespuestaUsuarioAdmin(admin.ModelAdmin):
     list_display = ['pregunta', 'respuesta']
-    search_fields = ['pregunta', 'respuesta']
+    list_filter = ['pregunta', 'respuesta']
 admin.site.register(models.RespuestaUsuario, RespuestaUsuarioAdmin)
+
+
+class RespuestaUsuarioInlineAdmin(admin.TabularInline):
+    model = models.Valoracion.respuestas.through
+    extra = 1
 
 
 class ValoracionAdmin(admin.ModelAdmin):
     list_display = ['fecha', 'clasificacion', 'get_respuestas']
-    search_fields = ['fecha', 'clasificacion', 'respuestas']
+    search_fields = ['fecha']
+    list_filter = ['respuestas', 'clasificacion']
+    
+    fields = ['clasificacion']
+    inlines = [RespuestaUsuarioInlineAdmin]
     
     def get_respuestas(self, obj):
         return ", ".join([str(respuesta_usuario) for respuesta_usuario in obj.respuestas.all()])
@@ -21,6 +29,7 @@ admin.site.register(models.Valoracion, ValoracionAdmin)
 
 class DiagnosticoAdmin(admin.ModelAdmin):
     list_display = ['paciente', 'medico', 'valoracion_inicial', 'fecha']
-    search_fields = ['paciente', 'medico', 'valoracion_inicial', 'fecha']
+    search_fields = ['fecha']
+    list_filter = ['paciente', 'medico', 'valoracion_inicial']
 admin.site.register(models.Diagnostico, DiagnosticoAdmin)
     
