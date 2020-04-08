d3256cd27a4ab8392316dd8f382627bb02543150
Virgilio <vpadillar8@gmail.com>
2020-04-08 13:57:56 -0500

    comentando archivo view para anular el admin como inicial

--- coviudc/views.py
+++ coviudc/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -9,3 +10,4 @@ class IndexAdmin(TemplateView):
         return redirect('/admin/')
     # end def
 # end class
+"""
